93dad18f6f8e4eb944bb9f28e2afd399a4a14d89
Richard Cohen <cohenrichard07@gmail.com>
2026-07-30 12:18:03 +0200

    fix: allow public Next assets to be crawled

--- app/robots.ts
+++ app/robots.ts
@@ -30,7 +30,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/api/', '/admin/', '/_next/'],
+        disallow: ['/api/', '/admin/'],
       },
       ...aiRules,
     ],
